df0c54fc1d4be5d40ff2a68c5a34323734ef8f5b
Dennis-Phamj <datpq.mmi@gmail.com>
2023-04-20 16:52:31 +0700

    second step

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import footer_img from "./assets/icons_assets/";
+
+export default function Footer() {
+  return (
+    <footer>
+      <img src="" alt="" />
+    </footer>
+  );
+}
---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/home">home</a>
+        </li>
+        <li>
+          <a href="/about">about</a>
+        </li>
+        <li>
+          <a href="/blog">menu</a>
+        </li>
+        <li>
+          <a href="/blog">reservations</a>
+        </li>
+        <li>
+          <a href="/blog">order online</a>
+        </li>
+        <li>
+          <a href="/blog">login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
